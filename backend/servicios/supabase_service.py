1a9fa82c0dd670939aa0ebe96248bf0ced0f43a2
BichoBolak <125624116+Darkingn@users.noreply.github.com>
2025-10-10 17:39:21 -0500

    Update supabase_service.py
    Agregue la URL y la clave API de supabase

--- backend/servicios/supabase_service.py
+++ backend/servicios/supabase_service.py
@@ -4,7 +4,7 @@ import os
 
 load_dotenv()
 
-SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+SUPABASE_URL = os.getenv("https://wigmurzhkogmzgdczjmb.supabase.co")
+SUPABASE_KEY = os.getenv("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndpZ211cnpoa29nbXpnZGN6am1iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjAxMTA0NDgsImV4cCI6MjA3NTY4NjQ0OH0.l0Gj1iJ3COqMXMgOPVmnVJhr01YkORw6Q3-hjAbtCEg")
 
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
