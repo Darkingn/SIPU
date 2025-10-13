1ad37ab53e1d72dae468d8138430bf81dd838280
llnard <bryanguaranda17@gmail.com>
2025-10-13 08:58:43 -0500

    lol

--- backend/modelos/Usuario.py
+++
@@ -1,9 +0,0 @@
-class Estudiante:
-    def __init__(self, id, nombre, correo, carrera):
-        self.id = id
-        self.nombre = nombre
-        self.correo = correo
-        self.carrera = carrera
-
-    def __str__(self):
-        return f"{self.nombre} ({self.carrera})"
